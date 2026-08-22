26b441f6b88babb9022d4c2d8440dac67ff7f6ce
nico100001-droid <nico_100_001@hotmail.com>
2026-08-22 01:36:24 -0300

    Update juegos.js

--- js/juegos.js
+++ js/juegos.js
@@ -5,29 +5,22 @@ const juegos = {
     // =========================================================
 
     nes: [
-
-        {
-            nombre: "Invaders",
+        {   nombre: "Invaders",
             core: "fceumm",
-
-            rom:
-                "https://raw.githubusercontent.com/retrobrews/nes-games/master/invaders.nes",
-
-            imagen:
-                "https://placehold.co/400x300/111/fff?text=Invaders"
+            rom: "https://raw.githubusercontent.com/retrobrews/nes-games/master/invaders.nes",
+            imagen: "https://placehold.co/400x300/111/fff?text=Invaders"
         },
-
+         {   nombre: "Mario 3",
+            core: "fceumm",
+            rom: "https://raw.githubusercontent.com/Kautenja/gym-super-mario-bros/master/gym_super_mario_bros/_roms/super-mario-bros-3.nes",
+            imagen: "https://i.blogs.es/1d386d/screenshot_149/1366_2000.webp?text=Invaders"
+        },
         {
             nombre: "Super Mario Bros.",
             core: "fceumm",
-
-            rom:
-                "https://raw.githubusercontent.com/Kautenja/gym-super-mario-bros/master/gym_super_mario_bros/_roms/super-mario-bros.nes",
-
-            imagen:
-                "https://placehold.co/400x300/111/fff?text=Super+Mario+Bros"
+            rom: "https://raw.githubusercontent.com/Kautenja/gym-super-mario-bros/master/gym_super_mario_bros/_roms/super-mario-bros.nes",
+            imagen: "https://emubrowser.com/sites/default/files/styles/large/public/boxart/super-mario-bros_1.webp?text=Super+Mario+Bros"
         }
-
     ],
 
 
